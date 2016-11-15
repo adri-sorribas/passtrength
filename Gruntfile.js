55c060ca18554719f78941ed65a60ad85559800d
adri-sorribas <adriansorribasherenas@gmail.com>
2016-11-15 15:38:55 +0100

    Add passtrength.js

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function( grunt ) {
+
+	grunt.initConfig( {
+
+		// Import package manifest
+		pkg: grunt.file.readJSON( "package.json" ),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			options: {
+				banner: "<%= meta.banner %>"
+			},
+			dist: {
+				src: [ "src/jquery.passtrength.js" ],
+				dest: "dist/js/jquery.passtrength.js"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: [ "src/jquery.passtrength.js", "test/**/*" ],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		jscs: {
+			src: "src/**/*.js",
+			options: {
+				config: ".jscsrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			dist: {
+				src: [ "dist/js/jquery.passtrength.js" ],
+				dest: "dist/js/jquery.passtrength.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// CoffeeScript compilation
+		coffee: {
+			compile: {
+				files: {
+					"dist/jquery.passtrength.js": "src/jquery.passtrength.coffee"
+				}
+			}
+		},
+
+		// watch for changes to source
+		// Better than calling grunt a million times
+		// (call 'grunt watch')
+		watch: {
+			files: [ "src/*", "test/**/*" ],
+			tasks: [ "default" ]
+		}
+
+	} );
+
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-jscs" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-coffee" );
+	grunt.loadNpmTasks( "grunt-contrib-watch" );
+
+	grunt.registerTask( "travis", [ "jshint" ] );
+	grunt.registerTask( "lint", [ "jshint", "jscs" ] );
+	grunt.registerTask( "build", [ "concat", "uglify" ] );
+	grunt.registerTask( "default", [ "jshint", "build" ] );
+};
